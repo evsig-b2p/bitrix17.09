48b8a910774c87e63f985650f4d1e9479b36a085
best2pay <github@best2pay.net>
2022-03-24 11:24:08 +0300

    user email

--- best2pay/payment.php
+++ best2pay/payment.php
@@ -54,26 +54,7 @@ try {
     $userEmail = '';
     
     if($ORDER) {
-        try {
-            /** @var \Bitrix\Sale\PropertyValueCollection $propertyCollection */
-            if($propertyCollection = $ORDER->getPropertyCollection()) {
-                if($propUserEmail = $propertyCollection->getUserEmail()) {
-                    $userEmail = $propUserEmail->getValue();
-                } else {
-                    /** @var \Bitrix\Sale\PropertyValue $orderProperty */
-                    foreach($propertyCollection as $orderProperty) {
-                        if($orderProperty->getField('CODE') == 'EMAIL') {
-                            $userEmail = $orderProperty->getValue();
-                            break;
-                        }
-                    }
-                }
-            }
-        } catch (Exception $ex) {
-            if (!$userEmail) {
-                $userEmail = $USER->GetEmail();
-            }
-        }
+        $userEmail = $USER->GetEmail();
     } else {
         throw new Exception('Order is not exist');
     }
